6126aef4f77da2f8d0f8c0351d4c430d3f302501
Ahsan Khizar <ahsankhizar1075@gmail.com>
2025-02-02 14:54:12 +0500

    Adding Responsive Design

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ export const Footer = () => {
       <Container>
         <Row className="align-items-center">
           <MailchimpForm />
-          <Col size={12} sm={6}>
+          <Col size={6} sm={6}>
             <img src={logo} alt="Logo" />
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
