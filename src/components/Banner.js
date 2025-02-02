5b1b2cf037dfbeb61c775b73329ccfffbf8b5332
Ahsan Khizar <ahsankhizar1075@gmail.com>
2025-02-02 17:16:25 +0500

    SEO Optimization

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,6 +5,7 @@ import { ArrowRightCircle } from 'react-bootstrap-icons';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
+
 export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
